872385e44ad1e7f61eec038f094f03a252ae94ab
Jeremy dain <37037667+jaydainn@users.noreply.github.com>
2020-04-20 11:14:37 +0200

    updatemvc

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php
 
 session_start();
+include_once 'models/header.php';
+include_once('models/navbar.php');
 
 if(isset($_GET['page'])){
     if($_GET['page']=='cours'){
-        include_once "./views/cours.php";
+        include_once "views/cours.php";
     }
 
 }
 else{
-    include_once "./views/accueil.php";
+    include_once "views/accueil.php";
 }
+
+include_once "models/footer.php";
--- models/navbar.php
+++ models/navbar.php
@@ -1,25 +1,36 @@
-<nav class="navbar navbar-expand-lg navbar-light ">
-    <a class="navbar-brand" href="#"><img src="./images/logoweb.svg"></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+<!DOCTYPE html>
+<html lang="en">
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+<head>
+  
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light ">
+        <a class="navbar-brand" href="#"><img src="./images/logoweb.svg"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+            </ul>
+        </div>
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <li class="nav-item">
+                <a href="index.php?page=cours"><i class="fas fa-book-open icon fa-lg"></i></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a href=""><i class="fas fa-user-circle icon fa-lg"></i></a>
             </li>
         </ul>
-    </div>
-    <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-            <a href="index.php?page=cours"><i class="fas fa-book-open icon fa-lg"></i></a>
-        </li>
-        <li class="nav-item">
-            <a href=""><i class="fas fa-user-circle icon fa-lg"></i></a>
-        </li>
-    </ul>
-</nav>
+    </nav>
+</body>
+
+</html>
