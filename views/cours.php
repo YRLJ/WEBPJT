1b59b1c6b2368447d4099668847ee215a907ae28
LemuelFalret <lemuelfalret@gmail.com>
2020-04-20 13:43:35 +0200

    Page Cours

--- views/cours.php
+++ views/cours.php
@@ -6,8 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nom - Cours</title>
 
-    <!-- CSS Bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
 
     <!-- CSS Navbar -->
@@ -23,13 +21,108 @@
 </head>
 
 <body>
-    <?php include_once "./models/navbar.php"; ?>
+
+    <div class="touslescours">
+        <a href="">
+            <div class="cours row">
+                <div class="col-8" id="titre">
+                    <h3>Cours de proba</h3>
+                </div>
+                <div class="col-4" id="note">
+                    <p>Appréciations des élève (note sur 10 ou étoiles sur 5)</p>
+                </div>
+                <div class="col-4" id="duree">
+                    <p>Durée approximative</p>
+                </div>
+                <div class="col-8" id="resume">
+                    <p>Résumé du cours</p>
+                </div>
+            </div>
+        </a>
+        <a href="">
+            <div class="cours row">
+                <div class="col-8" id="titre">
+                    <h3>Cours de probabilité</h3>
+                </div>
+                <div class="col-4" id="note">
+                    <p>8/10</p>
+                </div>
+                <div class="col-4" id="duree">
+                    <p>45min</p>
+                </div>
+                <div class="col-8" id="resume">
+                    <p>Explication des probabilités et exercices.</p>
+                </div>
+            </div>
+        </a>
+        <a href="">
+            <div class="cours row">
+                <div class="col-8" id="titre">
+                    <h3>Cours sur les molécules/h3>
+                </div>
+                <div class="col-4" id="note">
+                    <p>7.5/10</p>
+                </div>
+                <div class="col-4" id="duree">
+                    <p>15min</p>
+                </div>
+                <div class="col-8" id="resume">
+                    <p>Explication du princie de molécules.</p>
+                </div>
+            </div>
+        </a>
+        <a href="">
+            <div class="cours row">
+                <div class="col-8" id="titre">
+                    <h3>Cours de CSS</h3>
+                </div>
+                <div class="col-4" id="note">
+                    <p>2/10</p>
+                </div>
+                <div class="col-4" id="duree">
+                    <p>45min</p>
+                </div>
+                <div class="col-8" id="resume">
+                    <p>Synthèse explicative du CSS</p>
+                </div>
+            </div>
+        </a>
+        <a href="">
+            <div class="cours row">
+                <div class="col-8" id="titre">
+                    <h3>...</h3>
+                </div>
+                <div class="col-4" id="note">
+                    <p>...</p>
+                </div>
+                <div class="col-4" id="duree">
+                    <p>...</p>
+                </div>
+                <div class="col-8" id="resume">
+                    <p>...</p>
+                </div>
+            </div>
+        </a>
+        <a href="">
+            <div class="cours row">
+                <div class="col-8" id="titre">
+                    <h3>...</h3>
+                </div>
+                <div class="col-4" id="note">
+                    <p>...</p>
+                </div>
+                <div class="col-4" id="duree">
+                    <p>...</p>
+                </div>
+                <div class="col-8" id="resume">
+                    <p>...</p>
+                </div>
+            </div>
+        </a>
 
 
-<a class="btn-grad">Proposer cours</a>
-<a></a>
+    </div>
 
-    <?php include_once "./models/footer.php"; ?>
 </body>
 
 </html>
