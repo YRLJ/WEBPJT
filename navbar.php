a3e0c4b6d780b21dd7e03b34e2e8ff230ba26c5b
Thefatmax <63652408+Thefatmax@users.noreply.github.com>
2020-04-26 16:41:15 +0200

    ça va bien ?
    coucou

---
+++ navbar.php
@@ -0,0 +1,27 @@
+<?php
+//Yanis FATMI was here
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/4dded3e0b7.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="navbar.css">
+    <title>Navbar</title>
+</head>
+<body>
+    <nav class="navbar">
+        <a href="#" class="brand-title"><img height="35" witdh="11.7" src="logo.svg"></a>
+        <div class="navbar-links">
+            <ul>
+                <li><a href="#"><i class="fas fa-book-open fa-2x"></i></a></li>
+                <li><a href="#"><i class="fas fa-user fa-2x"></i></a></li>
+            </ul>
+        </div>
+    </nav>
+</body>
+</html>
