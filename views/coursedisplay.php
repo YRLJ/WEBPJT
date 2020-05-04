b7a1b87bdd57b140bab88c6631361f6200307dd4
Jeremy dain <37037667+jaydainn@users.noreply.github.com>
2020-05-04 11:46:49 +0200

    course

--- views/coursedisplay.php
+++ views/coursedisplay.php
@@ -8,52 +8,57 @@
 
 <body>
 
-<?php
-if(! isset($_SESSION)){
-    session_start();
-}
+    <?php
+    if (!isset($_SESSION)) {
+        session_start();
+    }
 
-include_once '../controller/courseController.php';
+    include_once './controller/courseController.php';
 
-$course = getCourseById($_GET['id']);
+    $course = getCourseById($_GET['id']);
 
- ?>
 
-    <h1 class="text-center"><?php echo $course['title'] ?> </h1>
-    <div class="container">
 
-        <div class="container course text-center">
+
+
+    echo  "<h1 class=\"text-center\">" .$course[0]['subject'].": ". $course[0]['title'] . " </h1>
+    <div class=\"container\">
+
+        <div class=\"container course text-center\">
             <br>
-            <br>
-            <?php if ($course['url'] != null) {
-                $split = str_split($course['url'], ".");
-                $ext = $split[1];
-                if ($ext == "pdf") {
-                    echo "<iframe class=\" rounded \" width=\"60%\" height=\"900px\" src=\"". $course['url'] . "\"></iframe>";
-                }
-                if ($ext == "mp4" || $ext =="mov" || $ext == "avi"){
-                    echo "<video src=\"".$course['url']."\"></video>";
-                }
-                if ($ext == "jpg" || $ext == "jpeg" || $ext == "png"){
-                    echo "<img src=\"".$course['url']."\">";
-                }
-            }
-            ?>
+            <br>";
+    if ($course[0]['url'] != null) {
+        $split = explode(".",$course[0]['url']);
+        
+        $ext = end($split);
+        if ($ext == "pdf") {
+            echo "<iframe class=\" rounded \" width=\"60%\" height=\"900px\" src=\"" . $course[0]['url'] . "\"></iframe>";
+        }
+        if ($ext == "mp4" || $ext == "mov" || $ext == "avi") {
+            echo "<video src=\"" . $course[0]['url'] . "\"></video>";
+        }
+        if ($ext == "jpg" || $ext == "jpeg" || $ext == "png") {
+            echo "<img src=\"" . $course[0]['url'] . "\">";
+        }
+    }
+    
 
-        </div>
-        <br>
-        <br>
-        <div class="container rounded text-justify content">
-            <p> <?php echo $course['content'] ?> </p>
-            
-        </div>
-        <br>
-        <br>
-        <div class="container buttons text-center">
-            <a href="#" class="btn btn-purple">Previous</a>
-            <a href="#" class="btn btn-purple">Quiz</a>
-            <a href="#" class="btn btn-purple">Next</a>
-        </div>
+    echo "</div>
+    <br>
+    <br>
+    <div class=\"container rounded text-justify content\">
+        <p>".$course[0]['content']."   </p>
+
+    </div>";
+    ?>
+    <br>
+    <br>
+    <div class="container buttons text-center">
+        <a href="#" class="btn btn-purple">Previous</a>
+        <a href="#" class="btn btn-purple">Quiz</a>
+        <a href="#" class="btn btn-purple">Next</a>
+    </div>
+</div>
 
 
 </body>
