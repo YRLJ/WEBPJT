dd431d74a344fc9d9334dd4a51aadbfda2ecfa27
LemuelFalret <lemuelfalret@gmail.com>
2020-05-04 15:44:50 +0200

    index

--- index.php
+++ index.php
@@ -6,6 +6,66 @@ include_once './models/loader.php';
 include_once './models/navbar.php';
 
 if (isset($_GET['page'])) {
+    switch ($_GET['page']) {
+        case 'createquiz':
+            include_once './views/quizcreator.php';
+            break;
+
+        case 'cours':
+            include_once "./views/cours.php";
+            break;
+
+        case 'coursdisplay':
+            include_once "./views/coursedisplay.php";
+            break;
+
+        case 'ajouter':
+            include_once './views/create_course.php';
+            break;
+
+        case 'login':
+            if (isset($_SESSION['type']) && ($_SESSION['type'] == "admin" || $_SESSION['type'] == "user")) {
+                include_once './views/myaccount.php';
+            } else {
+                include_once './views/login.php';
+            }
+            break;
+
+        case 'logout':
+            include_once './views/logout.php';
+            break;
+
+        case 'signup':
+            if (isset($_SESSION['type']) && ($_SESSION['type'] == "admin" || $_SESSION['type'] == "user")) {
+                include_once './views/myaccount.php';
+            } else {
+                include_once './views/signup.php';
+            }
+            break;
+
+        case 'connexion':
+            include_once './controller/accountController.php';
+            connexion();
+            break;
+
+        case "creatAccount":
+            include_once './controller/accountController.php';
+            creatAccount();
+            break;
+
+        case "proposercours":
+            include_once './views/proposer_cours.php';
+            break;
+
+        case "administrateur":
+            if ($_SESSION['type'] == "admin") {
+                include_once './views/administrateur.php';
+            }
+            break;
+    }
+
+    /*
+
     if ($_GET['page'] == 'createquiz') {
         include_once 'views/quizcreator.php';
     }
@@ -35,6 +95,7 @@ if (isset($_GET['page'])) {
             include_once './views/signup.php';
         }
     }
+    
     if ($_GET['page'] == 'connexion') {
         include_once './controller/accountController.php';
         connexion();
@@ -46,6 +107,7 @@ if (isset($_GET['page'])) {
     if ($_GET['page'] == "proposercours") {
         include_once './views/proposer_cours.php';
     }
+    */
 } else {
     include_once "views/accueil.php";
 }
