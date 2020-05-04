ea7a9af4877be18ad2890b8a033c479bf5aaff21
LemuelFalret <lemuelfalret@gmail.com>
2020-05-04 14:09:07 +0200

    pour yanis

--- views/accueil.php
+++ views/accueil.php
@@ -12,10 +12,17 @@
 
 <body>
 
-    <div class="titre">
+    
+    <?php if (isset($_SESSION['type']) && ($_SESSION['type'] == "admin" || $_SESSION['type'] == "user")) {
+        echo "connecté";
+    } else {
+        echo "pas connecté";
+        echo "<div class=\"titre\">
         <h1>Bienvenue sur notre plateforme</h1>
         <p>Notre but et notre volonté est de donner l'accés à tous les cours à tous types de personnes.</p>
-    </div>
+    </div>";
+    }
+    ?>
 
 
 
