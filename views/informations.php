81f9b5699c8acd49322654e89218cd20482ba41e
LemuelFalret <lemuelfalret@gmail.com>
2020-05-06 20:55:38 +0200

    ok

--- views/informations.php
+++ views/informations.php
@@ -1,23 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CoEd - Informations</title>
-    <link rel="stylesheet" href="./styles/styleinformations.css">
+    <link rel="stylesheet" href="./styles/styleinformations.css"><!-- On fait un lien avec le style de la page -->
 
 </head>
-<body>
-    <h1>Informations sur CoEd</h1>
 
-    <div class="aboutus">
-    <h2>À propos de nous</h2>
-    <p>CoEd est une plateforme gratuite en ligne qui permet à tous d'obtenir un enseignement de qualité.</p>
+<body>
+    <h1>Informations sur CoEd</h1><!-- Titre page -->
+
+    <div class="separation"></div><!-- Séparateur titre/page-->
+
+    <div class="aboutus"><!-- Div à propos -->
+        <img src="./images/informations/1.jpg">
+        <h2>À propos de nous</h2>
+        <p>CoEd est une plateforme gratuite en ligne qui permet à tous d'obtenir un enseignement de qualité.</p>
     </div>
 
-
-    <h2>Contact</h2>
-    <p>Tel. : +33 5 65 50 21 33</p>
-    <p>Mail : contact@coed.fr</p>
+    <div class="contact"><!-- Div à contact -->
+        <h2>Contact</h2>
+        <p>Tel. : +33 5 65 50 21 33</p>
+        <p>Mail : <a href="mailto:contact@coed.fr">contact@coed.fr</a>
+        </p>
+    </div>
 </body>
+
 </html>
--- views/myaccount.php
+++ views/myaccount.php
@@ -11,38 +11,44 @@
 
 <body>
     <div class="tous">
-    <h1>Mon compte</h1><!-- Titre de la page-->
+        <h1>Mon compte</h1><!-- Titre de la page-->
 
-    <h2>Mes informations</h2><!-- Première partie de la page : les informations du compte -->
-    <p>Bienvenue, <?php echo $_SESSION['firstname'] ?> <?php echo $_SESSION['lastname'] ?>. Vous êtes actuellement sur la page mon compte dont voici vos identifiants : <?php echo $_SESSION['username'] ?></p><!-- Balise PHP qui écrit les informations du compte-->
+        <div class="separation"></div><!-- Séparateur titre/page-->
 
-    <!-- Si le compte est un compte administrateur alors on lui permet d'accéder au dashboard admin -->
-    <?php
-    if ($_SESSION['type'] == 'admin') {
-        echo "Vous faîtes partie de notre équipe d'administrateur, vous pouvez alors accéder à <a href=\"index.php?page=administrateur\">notre page administrateur</a> où vous pouvez valider ou non les cours qui sont proposés.";
-    }
-    ?>
+        <div class="myinfo">
+            <h2>Mes informations</h2><!-- Première partie de la page : les informations du compte -->
+            <p>Bienvenue, <?php echo $_SESSION['firstname'] ?> <?php echo $_SESSION['lastname'] ?>. Vous êtes actuellement sur la page mon compte dont voici vos identifiants : <?php echo $_SESSION['username'] ?></p><!-- Balise PHP qui écrit les informations du compte-->
 
-    <h2>Les cours que je suis</h2><!-- Deuxième partie de la page : les cours que suit le compte actuel -->
+            <!-- Si le compte est un compte administrateur alors on lui permet d'accéder au dashboard admin -->
+            <?php
+            if ($_SESSION['type'] == 'admin') {
+                echo "Vous faîtes partie de notre équipe d'administrateur, vous pouvez alors accéder à <a href=\"index.php?page=administrateur\">notre page administrateur</a> où vous pouvez valider ou non les cours qui sont proposés.";
+            }
+            ?>
+        </div>
 
-    <?php
-    include_once './controller/courseController.php';
-    $idcourses = getAllIdAccountCourses();//on récupère tous les cours que suit un compte
-    echo "<div class=\"allcourses\">";
-    foreach ($idcourses as $idcourse) {
-        $course = getCourseById($idcourse['courseid']);
-        if ($course['valide'] == "oui") {
-            echo "<a href=\"index.php?page=coursdisplay&id=" . $course['courseid'] . "\">
+        <div class="mycourses">
+            <h2>Les cours que je suis</h2><!-- Deuxième partie de la page : les cours que suit le compte actuel -->
+
+            <?php
+            include_once './controller/courseController.php';
+            $idcourses = getAllIdAccountCourses(); //on récupère tous les cours que suit un compte
+            echo "<div class=\"allcourses\">";
+            foreach ($idcourses as $idcourse) {
+                $course = getCourseById($idcourse['courseid']);
+                if ($course['valide'] == "oui") {
+                    echo "<a href=\"index.php?page=coursdisplay&id=" . $course['courseid'] . "\">
                 <div class=\"course\">
                     <h3>" . $course['title'] . "</h3>
                     <p>" . $course['subject'] . "</p>
                 </div>
             </a>";
-        }
-    }
-    echo "</div>";
+                }
+            }
+            echo "</div>";
 
-    ?>
+            ?>
+        </div>
     </div>
 </body>
 
