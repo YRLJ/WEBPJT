2c0a3e33fc91e40f01519706896969fec30f3ad3
LemuelFalret <lemuelfalret@gmail.com>
2020-05-06 16:49:45 +0200

    test

--- views/informations.php
+++ views/informations.php
@@ -3,10 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CoEd - Contact</title>
+    <title>CoEd - Informations</title>
+    <link rel="stylesheet" href="./styles/styleinformations.css">
+
 </head>
 <body>
-    <h1>À propose de nous</h1>
-    <p>CoEd est une plateforme gratuite en ligne qui permet à tous </p>
+    <h1>Informations sur CoEd</h1>
+
+    <div class="aboutus">
+    <h2>À propos de nous</h2>
+    <p>CoEd est une plateforme gratuite en ligne qui permet à tous d'obtenir un enseignement de qualité.</p>
+    </div>
+
+
+    <h2>Contact</h2>
+    <p>Tel. : +33 5 65 50 21 33</p>
+    <p>Mail : contact@coed.fr</p>
 </body>
 </html>
