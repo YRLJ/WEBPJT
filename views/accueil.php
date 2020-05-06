c7130ddc4c5aa5600d1a7cd664336304b09c933e
Thefatmax <63652408+Thefatmax@users.noreply.github.com>
2020-05-06 12:08:12 +0200

    oui

--- views/accueil.php
+++ views/accueil.php
@@ -15,8 +15,8 @@
 
         <div class="div1" onmouseover="apparition()">
             <h2 class="txt1">Travailler à la maison n'a jamais été aussi simple.</h2>
-            <div class="div2">
-                <img style="height: 100px" src="images/accueil/2.jpg">
+            <div>
+                <img style="height: 700px" class="div2" id="invisible2" src="images/accueil/2.jpg">
             </div>
             <div>
                 <p id="invisible"><span style="font-size: 170%">Une solution simple d'utilisation, <br>pour faciliter la vie des élèves :</span><br>
@@ -27,12 +27,22 @@
                 </p>
             </div>
         </div>
+
+        <div class="div2">
+            <div class="conteneur">
+                <span class="txt2">Texte</span>
+                <span class="txt3">Texte</span>
+
+            </div>
+        </div>
     </div>
     <script>
         function apparition()
         {
             document.getElementById("invisible").style.color = "black";
             document.getElementById("invisible").style.transitionDuration = "2s";
+            document.getElementById("invisible2").style.opacity = "1";
+            document.getElementById("invisible2").style.transitionDuration = "1s";
         }
     </script>
 </body>
